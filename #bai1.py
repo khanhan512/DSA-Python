6aef6d0465184118f64a3659f37a19f3f7724129
khanhan512 <duongkhanhan512@gmail.com>
2023-12-28 15:17:47 +0700

    Create #bai1.py

---
+++ #bai1.py
@@ -0,0 +1,17 @@
+#bai1
+print("hello world")
+
+#bai2
+a="hello world"
+print(a)
+
+#bai3
+b="hello"
+c=" "
+d="world"
+print(b+c+d)
+
+#bai4
+def in_man_hinh(b+c+d):
+  print(b+c+d)
+in_man_hinh("Hello, world!")
